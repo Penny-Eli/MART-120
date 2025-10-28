78a4319a447af3df4b0d1998aa619a024149a49d
Penny-Eli <eli.penny@umontana.edu>
2025-10-27 23:58:02 -0600

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,38 @@
+function setup()
+
+{
+    createCanvas (500, 600);
+
+
+}
+
+function draw()
+
+{
+   
+background (120, 45, 78);
+fill ('navy');
+rect (225, 170, 50, 200);
+circle (250 , 100, 150);
+strokeWeight (10);
+point ( 225, 90);
+point (275,90);
+triangle (240, 110, 250, 100, 260, 110);
+line (225, 200, 150, 250);
+line (275, 200, 350, 250);
+line (235, 370, 220, 450);
+line (265, 370, 280, 450);
+textSize (30);
+text ('Navy Silhouette', 150, 500);
+textSize (10);
+text ('Eli Penny', 40, 30);
+
+
+
+
+
+
+
+
+
+}
